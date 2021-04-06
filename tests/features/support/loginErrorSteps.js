b16f90f505fb3cf5dfe72a9b022d3ba66d3306e8
neels198 <d18130574@mytudublin.ie>
2021-04-06 23:50:03 +0100

    Initial Commit
    Initial Commit with base structure and some helper functions.

---
+++ tests/features/support/loginErrorSteps.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert
+const { Given, When, Then } = require('@cucumber/cucumber')
+const And = Then
+const LoginErrorPage = require('../pages/loginErrorPage')
+const timeouts = require('../support/constants')
+
+Then('I should see login error message', async function () {
+  const loginErrorPage = new LoginErrorPage(this.driver)
+  await this.driver.wait(
+    async () => await loginErrorPage.getLoginErrorText(),
+    timeouts.STEP_TIMEOUTS.TIMEOUT
+  )
+})
